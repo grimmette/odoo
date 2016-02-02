524d7c7d626c30afee1ae002992b549ce96a17de
sv <sv@grimmette.com>
2016-02-02 20:05:12 +0300

    1

---
+++ iso4217/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (C) 2015 GRIMMETTE,LLC <info@grimmette.ru>
+
+import iso4217
---
+++ iso4217/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (C) 2015 GRIMMETTE,LLC <info@grimmette.com>
+
+{
+    'name': 'Codes and full name of currencies ISO 4217',
+    'version': '1.1',
+    'summary': 'Codes and full name of the currencies - ISO 4217',
+    'category': 'Localization/Account Charts',
+    'description': """
+Codes and full name of the currencies - ISO 4217.
+
+    """,
+    'author': 'GRIMMETTE,LLC',
+    'website': 'http://www.grimmette.com',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'iso4217_view.xml',
+        'data/iso4217.xml',
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+}
