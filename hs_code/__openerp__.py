d3f5b993efffe7aa4ba5e4062ed3e856f65b5b14
sv <sv@uwpc>
2016-12-02 14:06:50 +0300

    11

---
+++ hs_code/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'HS Code',
+    'version': '1.0',
+    'category': 'Stock',
+    'summary': 'Add field HS Code',
+    'description': """
+Add field HS Code.
+==============================================================
+
+""",
+    'author': 'GRIMMETTE,LLC',
+    'website': 'http://www.grimmette.com',
+    'depends': ['product'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_template_view.xml',
+    ],
+    'installable': True,
+}
---
+++ hs_code/models/product_template.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from openerp import api, models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    hs_code = fields.Char('HS Code')
+
+
